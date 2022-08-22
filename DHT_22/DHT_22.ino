e13dc17b9c59ff379866d20bd18245511261766c
Sai kiran J <saikiranj2002@gmail.com>
2022-08-22 17:57:57 +0530

    Interfacing DHT22 sensor to read Humidity and temperature to Arduion uno.

---
+++ DHT_22/DHT_22.ino
@@ -0,0 +1,29 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 2
+#define DHTTYPE    DHT22
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+
+void setup() 
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() 
+{
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  Serial.print("Temperature: ");
+  Serial.print(event.temperature);
+  Serial.println("°C");
+  dht.humidity().getEvent(&event);
+  Serial.print("Relative Humidity: ");
+  Serial.print(event.relative_humidity);
+  Serial.println("%");
+  delay(1000);
+}
